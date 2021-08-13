357974d4dc3f2f99f7c144e7ff92dbb750c5f77c
natasa227 <natasa.urosevic@programerika.com>
2021-08-13 15:51:37 +0200

    added react-confetti library

--- src/Score.js
+++ src/Score.js
@@ -1,39 +1,70 @@
 import React from "react";
-import ConfettiExplosion from "@reonomy/react-confetti-explosion";
 import { useDispatch } from "react-redux";
 import all_actions from "./redux/actions";
+import Confetti from "react-confetti";
+import { setTimeout } from "timers";
+import { useEffect, useState } from "react";
 
 const Score = ({ score }) => {
   const dispatch = useDispatch();
+  const [explode, setExplode] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setExplode(false);
+    }, 3000);
+  }, [explode]);
   return (
     <div>
       <div className="score">
         <h2>
           You got {score} points!!!
+          {}
           {score == 21 ? (
-            <ConfettiExplosion
-              force={0.7}
-              duration={3000}
-              particleCount={390}
-              floorHeight={950}
-              floorWidth={3000}
-            ></ConfettiExplosion>
+            <Confetti
+              width={window.innerWidth}
+              height={window.innerHeight}
+              tweenDuration={3000}
+              recycle={explode}
+              numberOfPieces={300}
+              wind={0.05}
+              gravity={0.2}
+              confettiSource={{ x: 100, y: 100, w: window.innerWidth, h: 500 }}
+              initialVelocityX={10}
+              initialVelocityY={10}
+            />
           ) : score == 13 ? (
-            <ConfettiExplosion
-              force={0.5}
-              duration={2000}
-              particleCount={300}
-              floorHeight={950}
-              floorWidth={2500}
-            ></ConfettiExplosion>
+            <Confetti
+              width={window.innerWidth}
+              height={window.innerHeight}
+              tweenDuration={3000}
+              recycle={explode}
+              numberOfPieces={200}
+              wind={0.05}
+              gravity={0.2}
+              confettiSource={{ x: 100, y: 100, w: window.innerWidth, h: 500 }}
+            />
+          ) : score == 8 ? (
+            <Confetti
+              width={window.innerWidth}
+              height={window.innerHeight}
+              tweenDuration={3000}
+              recycle={explode}
+              numberOfPieces={150}
+              wind={0.05}
+              gravity={0.2}
+              confettiSource={{ x: 100, y: 100, w: window.innerWidth, h: 500 }}
+            />
           ) : (
-            <ConfettiExplosion
-              force={0.3}
-              duration={1500}
-              particleCount={250}
-              floorHeight={950}
-              floorWidth={2000}
-            ></ConfettiExplosion>
+            <Confetti
+              width={window.innerWidth}
+              height={window.innerHeight}
+              tweenDuration={3000}
+              recycle={explode}
+              numberOfPieces={100}
+              wind={0.05}
+              gravity={0.2}
+              confettiSource={{ x: 100, y: 100, w: window.innerWidth, h: 500 }}
+            />
           )}
           <button
             onClick={() => dispatch(all_actions.input_actions.start_game())}
