81eecbab067e9aad25e85e4003057a2f11c79e00
Milan Uzelac <milan.uzelac@programerika.com>
2021-09-28 20:27:55 +0200

    changed tests for web-gejmika-model

---
+++ src/tests/WebGejmikaModel.test.js
@@ -0,0 +1,419 @@
+import { WebGejmikaModel } from "../model/WebGejmikaModel";
+
+// BEGIN of tests for function compareCode()
+
+// customized object for testing purposes
+const test21Points = {
+  attempts: [],
+  secretComb: ["H", "K", "T", "T"],
+  score: -1,
+  attpInProgress: [],
+};
+
+const wgmt = new WebGejmikaModel(test21Points);
+
+/**
+ *  @author Milan Uzelac
+ *  This test tests if a player has hit a correct combination in first attempt.
+ *  Function gets attemptInProgress as an argument, compares attemptInProgress with secret code,
+ *  if it matches, attemptOutcome array will be [2,2,2,2], player will get 21 points,
+ *  current attempt in progress object with attempt outcome will be placed in attempts array
+ */
+
+test("testing compareCode() with correct combination in first attempt", () => {
+  expect(wgmt.compareCode(["H", "K", "T", "T"])).toStrictEqual({
+    attempts: [
+      { attemptCode: ["H", "K", "T", "T"], attemptOutcome: [2, 2, 2, 2] },
+    ],
+    attpInProgress: [],
+    score: 21,
+    secretComb: ["H", "K", "T", "T"],
+  });
+});
+
+/**
+ *  @author Milan Uzelac
+ *  This test tests if a player has missed a correct combination in first attempt.
+ *  Function gets attemptInProgress as an argument, compares attemptInProgress with secret code
+ *  and if it doesn't match, attemptOutcome array will be [2,2,2,0] player will stay at -1 points,
+ *  current attempt in progress object with attempt outcome will be placed in attempts array
+ */
+test("testing compareCode() with incorect combination in first attempt", () => {
+  expect(wgmt.compareCode(["H", "K", "T", "S"])).toStrictEqual({
+    attempts: [
+      { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    ],
+    attpInProgress: [],
+    score: -1,
+    secretComb: ["H", "K", "T", "T"],
+  });
+});
+
+/**
+ * @author Milan Uzelac
+ *  This test tests if a player has hit a correct combination in fourth attempt.
+ *  Function gets attemptInProgress as an argument, compares attemptInProgress with secret code,
+ *  if it matches, attemptOutcome array will be [2,2,2,2] in fourth attempt,
+ *  player will get 13 points, current attempt in progress object with attempt
+ *  outcome will be placed in attempts array
+ */
+
+// customized object for testing purposes
+const test13Points = {
+  attempts: [
+    { attemptCode: ["H", "K", "T", "K"], attemptOutcome: [2, 2, 2, 1] },
+    { attemptCode: ["H", "K", "T", "K"], attemptOutcome: [2, 2, 2, 1] },
+    { attemptCode: ["H", "K", "T", "K"], attemptOutcome: [2, 2, 2, 1] },
+  ],
+  secretComb: ["H", "K", "T", "T"],
+  score: -1,
+  attpInProgress: [],
+};
+
+const wgmtTest13 = new WebGejmikaModel(test13Points);
+
+test("testing compareCode() with correct combination in fourth attempt", () => {
+  expect(wgmtTest13.compareCode(["H", "K", "T", "T"])).toStrictEqual({
+    attempts: [
+      { attemptCode: ["H", "K", "T", "K"], attemptOutcome: [2, 2, 2, 1] },
+      { attemptCode: ["H", "K", "T", "K"], attemptOutcome: [2, 2, 2, 1] },
+      { attemptCode: ["H", "K", "T", "K"], attemptOutcome: [2, 2, 2, 1] },
+      { attemptCode: ["H", "K", "T", "T"], attemptOutcome: [2, 2, 2, 2] },
+    ],
+    attpInProgress: [],
+    score: 13,
+    secretComb: ["H", "K", "T", "T"],
+  });
+});
+
+/**
+ * @author Milan Uzelac
+ * This test tests if a player has hit a correct combination in fifth attempt.
+ * Function gets attemptInProgress as an argument, compares attemptInProgress with secret code,
+ * if it matches, attemptOutcome array will be [2,2,2,2] in fifth attempt,
+ * player will get 8 points, current attempt in progress object with attempt
+ * outcome will be placed in attempts array
+ */
+
+// customized object for testing purposes
+const test8Points = {
+  attempts: [
+    { attemptCode: ["H", "K", "T", "K"], attemptOutcome: [2, 2, 2, 1] },
+    { attemptCode: ["H", "K", "T", "K"], attemptOutcome: [2, 2, 2, 1] },
+    { attemptCode: ["H", "K", "T", "K"], attemptOutcome: [2, 2, 2, 1] },
+    { attemptCode: ["H", "K", "T", "K"], attemptOutcome: [2, 2, 2, 1] },
+  ],
+  secretComb: ["H", "K", "T", "T"],
+  score: -1,
+  attpInProgress: [],
+};
+
+const wgmtTest8Points = new WebGejmikaModel(test8Points);
+
+test("testing compareCode() with correct combination in fifth attempt", () => {
+  expect(wgmtTest8Points.compareCode(["H", "K", "T", "T"])).toStrictEqual({
+    attempts: [
+      { attemptCode: ["H", "K", "T", "K"], attemptOutcome: [2, 2, 2, 1] },
+      { attemptCode: ["H", "K", "T", "K"], attemptOutcome: [2, 2, 2, 1] },
+      { attemptCode: ["H", "K", "T", "K"], attemptOutcome: [2, 2, 2, 1] },
+      { attemptCode: ["H", "K", "T", "K"], attemptOutcome: [2, 2, 2, 1] },
+      { attemptCode: ["H", "K", "T", "T"], attemptOutcome: [2, 2, 2, 2] },
+    ],
+    attpInProgress: [],
+    score: 8,
+    secretComb: ["H", "K", "T", "T"],
+  });
+});
+
+/**
+ * @author Milan Uzelac
+ * This test tests if a player has missed a correct combination in fifth attempt.
+ * Function gets attpInProgress as an argument, compares attemptInProgress with secret code,
+ * if it doesn't match, attemptOutcome array will be [2,2,2,0] in fifth attempt,
+ * target will be reached and player will get 0 points, current attempt in progress
+ * object with attempt outcome will be replaced from attpInProgress in attempts array
+ */
+
+// customized object for testing purposes
+const test0Points = {
+  attempts: [
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+  ],
+  secretComb: ["H", "K", "T", "T"],
+  score: -1,
+  attpInProgress: [],
+};
+
+const wgmtTest0Points = new WebGejmikaModel(test0Points);
+
+test("testing compareCode() with missed all combinations in all attempts", () => {
+  expect(wgmtTest0Points.compareCode(["H", "K", "T", "S"])).toStrictEqual({
+    attempts: [
+      { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+      { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+      { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+      { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+      { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    ],
+    attpInProgress: [],
+    score: 0,
+    secretComb: ["H", "K", "T", "T"],
+  });
+});
+
+/**
+ *  @author Milan Uzelac
+ *  This test tests if a sign is inside a secret code but in a wrong place
+ */
+
+test("testing compareCode() if a sign is inside a secret code but in a wrong place ", () => {
+  expect(wgmt.compareCode(["S", "K", "T", "H"])).toStrictEqual({
+    attempts: [
+      { attemptCode: ["S", "K", "T", "H"], attemptOutcome: [0, 2, 2, 1] },
+    ],
+    attpInProgress: [],
+    score: -1,
+    secretComb: ["H", "K", "T", "T"],
+  });
+});
+
+// END of tests for function compareCode()
+
+//BEGIN of tests for function isTargetReached()
+
+/**
+ * @author Milan Uzelac
+ * @return true
+ * Testing function isTargetReached with 5 total attempts
+ */
+
+// customized object for testing purposes
+const isTargetReached5Attempts = {
+  attempts: [
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+  ],
+  secretComb: ["H", "K", "T", "T"],
+  score: -1,
+  attpInProgress: [],
+};
+
+const wgmtIsTargetReached5Attemtps = new WebGejmikaModel(
+  isTargetReached5Attempts
+);
+
+test("testing isTargetReached() with five wrong attempts ", () => {
+  expect(
+    wgmtIsTargetReached5Attemtps.isTargetReached(
+      wgmtIsTargetReached5Attemtps.modelState.attempts
+    )
+  ).toBe(true);
+});
+
+/**
+ *  @author Milan Uzelac
+ *  @return false
+ *  Testing function isTargetReached with 4 total attempts
+ */
+
+// customized object for testing purposes
+const isTargetReached4Attempts = {
+  attempts: [
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+  ],
+  secretComb: ["H", "K", "T", "T"],
+  score: -1,
+  attpInProgress: [],
+};
+
+const wgmtIsTargetReached4Attemtps = new WebGejmikaModel(
+  isTargetReached4Attempts
+);
+
+test("testing isTargetReached() with four wrong attempts ", () => {
+  expect(
+    wgmtIsTargetReached4Attemtps.isTargetReached(
+      wgmtIsTargetReached4Attemtps.modelState.attempts
+    )
+  ).toBe(false);
+});
+
+/**
+ *  @author Milan Uzelac
+ *  @return true
+ *  Testing function isTargetReached with hit in fourth attempt
+ */
+
+// customized object for testing purposes
+const isTargetReachedCorrectCombination = {
+  attempts: [
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "T"], attemptOutcome: [2, 2, 2, 2] },
+  ],
+  secretComb: ["H", "K", "T", "T"],
+  score: 13,
+  attpInProgress: [],
+};
+
+const wgmtIsTargetReachedCorrectCombination = new WebGejmikaModel(
+  isTargetReachedCorrectCombination
+);
+
+test("testing isTargetReached() with hitted fourth combination ", () => {
+  expect(
+    wgmtIsTargetReachedCorrectCombination.isTargetReached(
+      wgmtIsTargetReachedCorrectCombination.modelState.attempts
+    )
+  ).toBe(true);
+});
+
+/**
+ *  @author Milan Uzelac
+ *  @return false
+ *  Testing isTargetReached() with empty attempts array
+ */
+
+// customized object for testing purposes
+const isTargetReachedEmptyArray = {
+  attempts: [],
+  secretComb: ["H", "K", "T", "T"],
+  score: 13,
+  attpInProgress: [],
+};
+
+const wgmtIsTargetReachedEmptyArray = new WebGejmikaModel(
+  isTargetReachedEmptyArray
+);
+
+test("testing isTargetReached() with empty attempts array ", () => {
+  expect(
+    wgmtIsTargetReachedEmptyArray.isTargetReached(
+      wgmtIsTargetReachedEmptyArray.modelState.attempts
+    )
+  ).toBe(false);
+});
+
+// END of tests for function IsTargetReached()
+
+// BEGIN of tests for function score()
+
+/**
+ *  @author Milan Uzelac
+ *  @returns object with score 21
+ *  Testing hit in second attempt
+ */
+
+// customized object for testing purposes
+const score21Points = {
+  attempts: [
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "T"], attemptOutcome: [2, 2, 2, 2] },
+  ],
+  secretComb: ["H", "K", "T", "T"],
+  score: 21,
+  attpInProgress: [],
+};
+
+const wgmtScore21Points = new WebGejmikaModel(score21Points);
+
+test("testing score() - 21 points 2 attempt", () => {
+  expect(
+    wgmtScore21Points.score(wgmtScore21Points.modelState.attempts)
+  ).toStrictEqual(21);
+});
+
+/**
+ * @author Milan Uzelac
+ * @returns object with score 13
+ * Testing fourth attempt
+ */
+
+// customized object for testing purposes
+const score13Points = {
+  attempts: [
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "T"], attemptOutcome: [2, 2, 2, 2] },
+  ],
+  secretComb: ["H", "K", "T", "T"],
+  score: 13,
+  attpInProgress: [],
+};
+
+const wgmtScore13Points = new WebGejmikaModel(score13Points);
+
+test("testing score() - 13 points 4 attempt", () => {
+  expect(
+    wgmtScore13Points.score(wgmtScore13Points.modelState.attempts)
+  ).toStrictEqual(13);
+});
+
+/**
+ * @author Milan Uzelac
+ * @returns object with score 8
+ * Testing fifth attempt
+ */
+
+// customized object for testing purposes
+const score8Points = {
+  attempts: [
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "T"], attemptOutcome: [2, 2, 2, 2] },
+  ],
+  secretComb: ["H", "K", "T", "T"],
+  score: 8,
+  attpInProgress: [],
+};
+
+const wgmtScore8Points = new WebGejmikaModel(score8Points);
+
+test("testing score() - 8 points 5 attempt", () => {
+  expect(
+    wgmtScore8Points.score(wgmtScore8Points.modelState.attempts)
+  ).toStrictEqual(8);
+});
+
+/**
+ * @author Milan Uzelac
+ * @returns object with score 0
+ * Missed all attempts
+ */
+
+// customized object for testing purposes
+const score0Points = {
+  attempts: [
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+    { attemptCode: ["H", "K", "T", "S"], attemptOutcome: [2, 2, 2, 0] },
+  ],
+  secretComb: ["H", "K", "T", "T"],
+  score: 8,
+  attpInProgress: [],
+};
+
+const wgmtScore0Points = new WebGejmikaModel(score0Points);
+
+test("testing score() - 0 points 5 attempt", () => {
+  expect(
+    wgmtScore0Points.score(wgmtScore0Points.modelState.attempts)
+  ).toStrictEqual(0);
+});
+
+// END of testing score() function
