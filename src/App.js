6d0a508bdf6fa5cca53fcb8978f0d6cfeca316de
Vladimir Djekic <vladimir.djekic@programerika.com>
2021-11-01 21:48:34 +0100

    Optimized css (removed layout issues)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import WebGejmika from "./lib/components/WebGejmika";
 
 const App = () => {
-  return <WebGejmika />;
+  return (
+    <div className="game-container">
+      <WebGejmika />
+    </div>
+  );
 };
 
 export default App;
