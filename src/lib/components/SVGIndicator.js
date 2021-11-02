356a0d16f1eb1463b0fd76d34ae4c9d6755b6be3
Vladimir Djekic <vladimir.djekic@programerika.com>
2021-11-02 20:40:24 +0100

    Changed createPie in SVGIndicator for more functional approach

--- src/lib/components/SVGIndicator.js
+++ src/lib/components/SVGIndicator.js
@@ -3,21 +3,16 @@ import styles from "./SVGIndicator.module.css";
 
 function createPie(cx, cy, r, colors) {
   const slices = colors.length;
-  var fromAngle, toAngle, fromCoordX, fromCoordY, toCoordX, toCoordY, d;
-  var paths = [];
-  for (var i = 0; i < slices; i++) {      
-    fromAngle = (i * 360) / slices;
-    toAngle = ((i + 1) * 360) / slices;
-    fromCoordX = cx + r * Math.cos((fromAngle * Math.PI) / 180);
-    fromCoordY = cy + r * Math.sin((fromAngle * Math.PI) / 180);
-    toCoordX = cx + r * Math.cos((toAngle * Math.PI) / 180);
-    toCoordY = cy + r * Math.sin((toAngle * Math.PI) / 180);
-
-    d = `M${cx},${cy} L${fromCoordX},${fromCoordY} A${r},${r} 0 0,1 ${toCoordX},${toCoordY}z`;
-
-    paths.push(<path key={i} d={d} fill={colors[i]}/>);
-  }
-  return paths;
+  return colors.map((color,i)=>{
+    const fromAngle = (i * 360) / slices;
+    const toAngle = ((i + 1) * 360) / slices;
+    const fromCoordX = cx + r * Math.cos((fromAngle * Math.PI) / 180);
+    const fromCoordY = cy + r * Math.sin((fromAngle * Math.PI) / 180);
+    const toCoordX = cx + r * Math.cos((toAngle * Math.PI) / 180);
+    const toCoordY = cy + r * Math.sin((toAngle * Math.PI) / 180);
+    const d = `M${cx},${cy} L${fromCoordX},${fromCoordY} A${r},${r} 0 0,1 ${toCoordX},${toCoordY}z`;
+    return <path key={i} d={d} fill={color}/>;
+  });
 }
 
 const SVGIndicator = ({ colors }) => {
