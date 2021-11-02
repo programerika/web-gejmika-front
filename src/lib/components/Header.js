454cdbade844ef4fcf3b6bc8bff2e4cab4868300
Vladimir Djekic <vladimir.djekic@programerika.com>
2021-11-02 15:13:28 +0100

    Introducing CSS modules - Header component

--- src/lib/components/Header.js
+++ src/lib/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import questionMark from '../icons/question-mark.png';
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div className="header">
+    <div className={styles.header}>
       <h2>Webgejmika</h2>
-      <div className="help-wrapper">
+      {/*TODO move to Help component*/}
+      <div className={styles.helpWrapper}>
         <img className="input" src={questionMark} alt="help" />
       </div>
     </div>
